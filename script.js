43e984f8e47da961a536bfad092c6267d8c6d5bf
YUARDIAN <yuardian.music@gmail.com>
2026-06-26 01:59:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,29 @@ behavior:'smooth'
 });
 
 }
+const acc=document.querySelectorAll(".accordion");
 
+acc.forEach(item=>{
+
+item.addEventListener("click",function(){
+
+this.classList.toggle("active");
+
+let panel=this.nextElementSibling;
+
+if(panel.style.display==="block"){
+
+panel.style.display="none";
+
+}else{
+
+panel.style.display="block";
+
+}
+
+});
+
+});
 });
 
 });
